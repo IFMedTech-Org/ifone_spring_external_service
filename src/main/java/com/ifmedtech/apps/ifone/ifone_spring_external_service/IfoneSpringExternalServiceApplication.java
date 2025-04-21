78969ec3e664692f68e9fefef3cb010b80ba7a06
skkeskar2000 <saurabhkeskar2000@gmail.com>
2025-04-21 13:01:04 +0530

    First commit

---
+++ src/main/java/com/ifmedtech/apps/ifone/ifone_spring_external_service/IfoneSpringExternalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ifmedtech.apps.ifone.ifone_spring_external_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfoneSpringExternalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IfoneSpringExternalServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ifmedtech/apps/ifone/ifone_spring_external_service/IfoneSpringExternalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifmedtech.apps.ifone.ifone_spring_external_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfoneSpringExternalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
